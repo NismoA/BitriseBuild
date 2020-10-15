767ecaa1e52cc5cde9c7c64ea0e9a266dfe955f5
Akarapas Wongkaew <akarapaswongkaew@gmail.com>
2020-10-15 14:49:26 +0700

    init

---
+++ BitriseBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseBuild
+//
+//  Created by Akarapas Wongkaew on 15/10/2563 BE.
+//  Copyright © 2563 Akarapas Wongkaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
